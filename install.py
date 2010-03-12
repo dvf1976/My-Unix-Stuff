6136d1a46300a48fb181d5209b642143b92bc9af
Daniel Fisher <dvf1976@dhcp243-71.rdu.redhat.com>
2010-03-12 09:57:31 -0500

    install.py installs screenrc

--- install.py
+++ install.py
@@ -5,8 +5,15 @@ import os
 vimrc_location = "%s/vimrc" % os.getcwd()
 dot_vimrc_location = "%s/.vimrc" % os.environ['HOME']
 
+screenrc_location = "%s/screenrc" % os.getcwd()
+dot_screenrc_location = "%s/.screenrc" % os.environ['HOME']
+
 if os.path.exists(dot_vimrc_location):
     os.unlink(dot_vimrc_location) 
-#print vimrc_location + "\n" + dot_vimrc_location
 os.symlink(vimrc_location, dot_vimrc_location)
+
+if os.path.exists(dot_screenrc_location):
+    os.unlink(dot_screenrc_location) 
+#print screenrc_location + "\n" + dot_screenrc_location
+os.symlink(screenrc_location, dot_screenrc_location)
 
