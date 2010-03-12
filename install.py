22a7cf9c08dad96d3c735c7fd35cd739393000f6
Daniel Fisher <dvf1976@dhcp243-71.rdu.redhat.com>
2010-03-12 10:10:03 -0500

    Stupid syntax fixes that I should've run checks on before previous check-in

--- install.py
+++ install.py
@@ -10,13 +10,13 @@ dot_screenrc_location = "%s/.screenrc" % os.environ['HOME']
 
 if os.path.exists(dot_vimrc_location):
     os.unlink(dot_vimrc_location) 
-if os.path.exists(dot_vimrc_location) and not os.path.exists(os.readlink(dot_vimrc_location))
+if os.path.exists(dot_vimrc_location) and not os.path.exists(os.readlink(dot_vimrc_location)):
     os.unlink(dot_vimrc_location) 
 os.symlink(vimrc_location, dot_vimrc_location)
 
 if os.path.exists(dot_screenrc_location):
     os.unlink(dot_screenrc_location)
-if os.path.exists(dot_vimrc_location) and not os.path.exists(os.readlink(dot_screenrc_location)):
+if os.path.exists(dot_screenrc_location) and not os.path.exists(os.readlink(dot_screenrc_location)):
     os.unlink(dot_screenrc_location) 
 #print screenrc_location + "\n" + dot_screenrc_location
 os.symlink(screenrc_location, dot_screenrc_location)
