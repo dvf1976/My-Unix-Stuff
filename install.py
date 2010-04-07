62b3d9a44b2cca1056e22d81a4de5310aa77a0c8
Daniel Fisher <dvf1976@lingling.localdomain>
2010-04-07 09:48:48 -0400

    Added ssh_config file + installer

--- install.py
+++ install.py
@@ -8,6 +8,9 @@ dot_vimrc_location = "%s/.vimrc" % os.environ['HOME']
 screenrc_location = "%s/screenrc" % os.getcwd()
 dot_screenrc_location = "%s/.screenrc" % os.environ['HOME']
 
+source_sshconfig_location = "%s/ssh_config" % os.getcwd()
+destination_sshconfig_location = "%s/.ssh/config" % os.environ['HOME']
+
 bash_profile_append = "%s/bash_profile_append.bash" % os.getcwd()
 
 if os.path.exists(dot_vimrc_location) or (os.path.lexists(dot_vimrc_location) and not os.path.exists(os.readlink(dot_vimrc_location))):
@@ -17,6 +20,10 @@ os.symlink(vimrc_location, dot_vimrc_location)
 if os.path.exists(dot_screenrc_location) or (os.path.lexists(dot_screenrc_location) and not os.path.exists(os.readlink(dot_screenrc_location))):
     os.unlink(dot_screenrc_location)
 os.symlink(screenrc_location, dot_screenrc_location)
+
+if os.path.exists(destination_sshconfig_location) or (os.path.lexists(destination_sshconfig_location) and not os.path.exists(os.readlink(destination_sshconfig_location))):
+    os.unlink(destination_sshconfig_location)
+os.symlink(source_sshconfig_location, destination_sshconfig_location)
 
 if not os.popen('/bin/grep My-Unix-Stuff ~/.bashrc').read():
    os.popen('/bin/cat ' + bash_profile_append + ' >> ~/.bashrc')
