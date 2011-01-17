c421827afa12577c847b3a5737aa72b4d793a470
Daniel Fisher <dfisher@redhat.com>
2011-01-17 10:46:59 -0500

    Added some aliases from http://library.edgecase.com/git_immersion/lab_11.html

--- profile.d/git.sh
+++ profile.d/git.sh
@@ -12,3 +12,27 @@ fi
 git config --global core.autocrlf input
 git config --global core.safecrlf true
 
+# http://library.edgecase.com/git_immersion/lab_11.html
+# http://davidwalsh.name/git-aliases
+
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.hist 'log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
+git config --global alias.type 'cat-file -t'
+git config --global alias.dump 'cat-file -p'
+
+# http://library.edgecase.com/git_immersion/lab_11.html
+alias gs='git status '
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit'
+alias gd='git diff'
+alias go='git checkout '
+alias gk='gitk --all&'
+alias gx='gitx --all'
+
+alias got='git '
+alias get='git '
+
