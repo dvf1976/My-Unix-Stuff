bcce071f93c9829ed1a740c5116c6d43d3e8f522
Daniel Fisher <dfisher@redhat.com>
2011-01-17 09:37:30 -0500

    Added crlf git commands

--- profile.d/git.sh
+++ profile.d/git.sh
@@ -7,5 +7,8 @@ then
     echo 'on redhat host'
     git config --global user.email "dfisher@redhat.com"
 fi
-    
+
+# http://library.edgecase.com/git_immersion/lab_01.html
+git config --global core.autocrlf input
+git config --global core.safecrlf true
 
