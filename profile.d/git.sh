49289544546a014f1eb4cf1dddaacab8096c2aae
Daniel Fisher <dfisher@redhat.com>
2011-01-17 09:36:41 -0500

    If on redhat host, then I'm 'dfisher@redhat.com'

--- profile.d/git.sh
+++ profile.d/git.sh
@@ -1,4 +1,11 @@
 git config --global color.ui "auto"
 git config --global user.name "Daniel Fisher"
 git config --global user.email "dan@askingforthirds.org"
+hostname=`/bin/hostname`
+if [[ $hostname =~ 'redhat.com' ]]
+then
+    echo 'on redhat host'
+    git config --global user.email "dfisher@redhat.com"
+fi
+    
 
