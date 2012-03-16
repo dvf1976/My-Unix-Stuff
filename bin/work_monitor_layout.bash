fa7325f2e0193e351761e545675bb9778f366dc5
Daniel Fisher <dan@askingforthirds.org>
2012-03-16 07:35:31 -0400

    Make it work with martin

--- bin/work_monitor_layout.bash
+++ bin/work_monitor_layout.bash
@@ -1,3 +1,5 @@
-xrandr --output LVDS1 --right-of VGA1
+xrandr --output LVDS1 --left-of VGA1
+xrandr --output LVDS1 --mode 1440x900 --rate 60
+# xrandr --output LVDS1 --mode 1024x768 --rate 60
 # xrandr --output VGA1 --mode 1152x864 --rate 75
-xrandr --output VGA1 --mode 1280x960 --rate 60
+xrandr --output VGA1 --mode 1280x1024 --rotate left --rate 60
