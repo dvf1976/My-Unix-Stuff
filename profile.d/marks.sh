845ace3d46420b7ce4b1f55685b287d7c82568e9
Daniel Fisher <dan@askingforthirds.org>
2013-08-17 12:21:32 -0400

    Added autocompletion

--- profile.d/marks.sh
+++ profile.d/marks.sh
@@ -11,3 +11,12 @@ function unmark {
 function marks {
     ls -l $MARKPATH | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
 }
+
+function _jump {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local marks=$(find $MARKPATH -type l -printf "%f\n")
+    COMPREPLY=($(compgen -W '${marks[@]}' -- "$cur"))
+    return 0
+}
+complete -o default -o nospace -F _jump jump
+
