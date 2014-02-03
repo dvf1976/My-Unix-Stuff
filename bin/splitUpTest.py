35203267596a627eff21c2c9121d6573659cdca2
Daniel Fisher <dan@askingforthirds.org>
2014-02-03 08:16:43 -0500

    md5 the test contents so the name can stay the same

--- bin/splitUpTest.py
+++ bin/splitUpTest.py
@@ -2,6 +2,7 @@
 
 import os, sys
 import re
+import hashlib
 from glob import glob
 
 target_file = 'RemoteUtilsTest.cls'
@@ -28,23 +29,36 @@ testTemplateXML = """
 <ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">
     <apiVersion>29.0</apiVersion>
     <status>Active</status>
+    <packageVersions>
+        <majorNumber>1</majorNumber>
+        <minorNumber>10</minorNumber>
+        <namespace>AAA</namespace>
+    </packageVersions>
 </ApexClass>
 """
 
 for old_test_file in glob('AAA_*'):
     os.unlink(old_test_file)
 
-method_name_to_method_hash = {}
+m = hashlib.md5()
+
+md5_method_name_to_method_hash = {}
 for method in methods:
     method_lines = re.split(r'\n', method)
     top_of_method = method_lines[0]
     groups = re.search(r'(\w*)\(\s*\)', top_of_method)
-    method_name = groups.group(1).lower()
-    method_name_to_method_hash[method_name] = method
+    original_method_name = groups.group(1).lower()
+    m.update(method)
+    md5_method_name = m.hexdigest()
+    md5_method_name_to_method_hash[md5_method_name] = {
+        'method' : method,
+        'method_name' : original_method_name
+    }
 
-for (index, method_name) in enumerate(sorted(method_name_to_method_hash.keys())):
-    method = method_name_to_method_hash[method_name]
-    test_class_name = 'AAA_%03d_%s' % (index, method_name,)
+for (md5_method_name, struct) in md5_method_name_to_method_hash.items():
+    method = struct['method']
+    method_name = struct['method_name']
+    test_class_name = 'AAA_%s' % (md5_method_name,)
     test_class_name = test_class_name[:39]
     test_class_name = re.sub(r'_+$', '', test_class_name)
 
@@ -54,10 +68,14 @@ for (index, method_name) in enumerate(sorted(method_name_to_method_hash.keys()))
     testContents = testTemplate % (method_name, target_file, test_class_name, '\t', method)
     # print testContents
 
+    testXML = testTemplateXML
+
     f = open(test_class_file_name, 'w')
     f.write(testContents.strip())
     f.close()
 
     f = open(test_class_xml_file_name, 'w')
-    f.write(testTemplateXML.strip())
+    f.write(testXML.strip())
     f.close()
+
+    print test_class_name
